d8b6a813535d77b9e7fcb042051fa1ed06921bd8
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-08-25 02:10:14 +0530

    The basic message types

--- app/Paxos.hs
+++ app/Paxos.hs
@@ -1,3 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ExistentialQuantification #-}
+
+
 module Paxos where
+
+import Data.Binary
+import Data.Typeable
+
+import GHC.Generics
+
+newtype RoundNo = RoundNo Int deriving (Typeable, Generic)
+
+instance Binary RoundNo
+
+data Value a = Value a deriving (Typeable, Generic)
+
+instance Binary a => Binary (Value a)
+
+data Message a = Plebiscit                     -- Round Leader sends
+               | Allegiance (Value a, RoundNo) -- Witness responds
+               | Propose (Value a, RoundNo)    -- Leader sends the selected value
+               | Accept (Value a, RoundNo)     -- Witness sends to leader
+               | Decision (Value a, RoundNo)   -- Leader sends to witnesses
+               deriving (Typeable, Generic)
+
+instance Binary a => Binary (Message a)
 
 
